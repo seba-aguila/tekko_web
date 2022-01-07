1b5353e930dd96fc37ff7553d245f4938eafac20
Sebam_ <seaguila2019@inf.udec.cl>
2022-01-07 00:15:18 -0300

    Animacion Navbar

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -13,10 +13,7 @@ export const Nav = styled.nav`
   position: sticky;
   top: 0;
   z-index: 10;
-
-  @media screen and (max-width: 960px) {
-    transition: 0.8s all ease;
-  }
+  transition: 0.8s all ease;
 `;
 
 export const NavbarContainer = styled.div`
